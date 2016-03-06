b835f1d9d40fa43642cf5c70e6d57c5046e9279f
Илья Сапсырин <nrgentoo@gmail.com>
2016-03-06 14:36:13 +0300

    commit

---
+++ app/src/main/java/com/nrgentoo/tweeterstream/store/TimeLineStore.java
@@ -0,0 +1,17 @@
+package com.nrgentoo.tweeterstream.store;
+
+import android.support.annotation.Nullable;
+
+import com.twitter.sdk.android.core.models.Tweet;
+
+import java.util.List;
+
+/**
+ * Interface for timeline
+ */
+public interface TimelineStore {
+
+    String ID = "TimelineStore";
+
+    List<Tweet> getHomeTimeline(@Nullable Long sinceId, @Nullable Long maxId);
+}
